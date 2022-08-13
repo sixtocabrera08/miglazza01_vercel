d7a5d32f26ab0793bf78336c2196c61666f6ec23
sixtocabrera08 <111189094+sixtocabrera08@users.noreply.github.com>
2022-08-13 16:51:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+app.use(express.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
